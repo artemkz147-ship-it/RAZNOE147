4a0ede3a4fc669f66432334329a4c9823e70bfe3
artemkz147-ship-it <artemkz147@gmail.com>
2026-08-11 00:20:06 +1000

    Set runtime version 0.6.0

--- web/version.js
+++ web/version.js
@@ -1,4 +1,4 @@
 (() => {
-  if (window.UMK3_DATA) window.UMK3_DATA.version = '0.5.0';
-  window.UMK3_BUILD = { version: '0.5.0', versionCode: 5, renderer: 'hd-atlas-v1' };
+  if (window.UMK3_DATA) window.UMK3_DATA.version = '0.6.0';
+  window.UMK3_BUILD = { version: '0.6.0', versionCode: 6, renderer: 'raster-pack-v2' };
 })();
