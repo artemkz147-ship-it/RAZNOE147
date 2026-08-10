bb1a63902ee2fbaf2755a9d6b1abafc9031e1166
artemkz147-ship-it <artemkz147@gmail.com>
2026-08-11 02:36:07 +1000

    Bump prerendered remaster to 0.7.0

--- web/version.js
+++ web/version.js
@@ -1,4 +1,10 @@
 (() => {
-  if (window.UMK3_DATA) window.UMK3_DATA.version = '0.6.0';
-  window.UMK3_BUILD = { version: '0.6.0', versionCode: 6, renderer: 'raster-pack-v2' };
+  if (window.UMK3_DATA) window.UMK3_DATA.version = '0.7.0';
+  window.UMK3_BUILD = {
+    version: '0.7.0',
+    versionCode: 7,
+    renderer: 'prerendered-48f-hd-v4-layered-stages',
+    mobileControls: 'multitouch-v2',
+    assetPolicy: 'baked-offline-cc0'
+  };
 })();
