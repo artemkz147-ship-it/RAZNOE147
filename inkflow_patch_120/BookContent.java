a43df4932b513d50f32a4c126c6177bf92dc7b47
artemkz147-ship-it <artemkz147@gmail.com>
2026-09-06 09:13:13 +0300

    Add Читай Всё book reader content model

---
+++ inkflow_patch_120/BookContent.java
@@ -0,0 +1,7 @@
+package app.inkflow.reader;
+
+public final class BookContent {
+    public final String title;
+    public final String html;
+    public BookContent(String title, String html){this.title=title;this.html=html;}
+}
